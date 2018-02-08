827a42a0e33d88e4dc8dd84538c23b647563ae5b
Soravit Beer Changpinyo <martbeerina@gmail.com>
2018-02-07 20:42:57 -0800

    simple tagger with pos and ner + config files

--- allennlp/models/simple_tagger.py
+++ allennlp/models/simple_tagger.py
@@ -13,7 +13,7 @@ from allennlp.modules import Seq2SeqEncoder, TimeDistributed, TextFieldEmbedder
 from allennlp.models.model import Model
 from allennlp.nn import InitializerApplicator, RegularizerApplicator
 from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
-from allennlp.training.metrics import CategoricalAccuracy
+from allennlp.training.metrics import CategoricalAccuracy, SpanBasedF1Measure
 
 
 @Model.register("simple_tagger")
@@ -40,12 +40,15 @@ class SimpleTagger(Model):
     def __init__(self, vocab: Vocabulary,
                  text_field_embedder: TextFieldEmbedder,
                  stacked_encoder: Seq2SeqEncoder,
+                 label_namespace: str = "labels",
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None) -> None:
         super(SimpleTagger, self).__init__(vocab, regularizer)
 
+        self.label_namespace = label_namespace
         self.text_field_embedder = text_field_embedder
-        self.num_classes = self.vocab.get_vocab_size("labels")
+        # self.num_classes = self.vocab.get_vocab_size("labels")
+        self.num_classes = self.vocab.get_vocab_size(label_namespace)
         self.stacked_encoder = stacked_encoder
         self.tag_projection_layer = TimeDistributed(Linear(self.stacked_encoder.get_output_dim(),
                                                            self.num_classes))
@@ -56,6 +59,7 @@ class SimpleTagger(Model):
                 "accuracy": CategoricalAccuracy(),
                 "accuracy3": CategoricalAccuracy(top_k=3)
         }
+        self.span_metric = SpanBasedF1Measure(vocab, tag_namespace=label_namespace)
 
         initializer(self)
 
@@ -104,7 +108,21 @@ class SimpleTagger(Model):
                                                                           sequence_length,
                                                                           self.num_classes])
 
-        output_dict = {"logits": logits, "class_probabilities": class_probabilities}
+        all_predictions = class_probabilities.cpu().data.numpy()
+        if all_predictions.ndim == 3:
+            predictions_list = [all_predictions[i] for i in range(all_predictions.shape[0])]
+        else:
+            predictions_list = [all_predictions]
+        predicted_tags = []
+        for predictions in predictions_list:
+            argmax_indices = numpy.argmax(predictions, axis=-1)
+            # cur_tags = [self.vocab.get_token_from_index(x, namespace=self.label_namespace)
+            #         for x in argmax_indices]
+            predicted_tags.append(argmax_indices)
+
+        output_dict = {"logits": logits, "tags": predicted_tags,
+                       "class_probabilities": class_probabilities, "mask": mask}
+        # output_dict = {"logits": logits, "tags": predicted_tags, "mask": mask}
 
         if tags is not None:
             loss = sequence_cross_entropy_with_logits(logits, tags, mask)
@@ -112,6 +130,13 @@ class SimpleTagger(Model):
                 metric(logits, tags, mask.float())
             output_dict["loss"] = loss
 
+            # Represent viterbi tags as "class probabilities" that we can
+            # feed into the `span_metric`
+            class_probabilities = logits * 0.
+            for i, instance_tags in enumerate(predicted_tags):
+                for j, tag_id in enumerate(instance_tags):
+                    class_probabilities[i, j, tag_id] = 1
+            self.span_metric(class_probabilities, tags, mask)
         return output_dict
 
     @overrides
@@ -119,6 +144,7 @@ class SimpleTagger(Model):
         """
         Does a simple position-wise argmax over each token, converts indices to string labels, and
         adds a ``"tags"`` key to the dictionary with the result.
+        """
         """
         all_predictions = output_dict['class_probabilities']
         all_predictions = all_predictions.cpu().data.numpy()
@@ -134,22 +160,35 @@ class SimpleTagger(Model):
             all_tags.append(tags)
         output_dict['tags'] = all_tags
         return output_dict
+        """
+
+        output_dict["tags"] = [
+            [self.vocab.get_token_from_index(tag, namespace=self.label_namespace)
+             for tag in instance_tags]
+            for instance_tags in output_dict["tags"]
+            ]
+
+        return output_dict
 
     @overrides
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
-        return {metric_name: metric.get_metric(reset) for metric_name, metric in self.metrics.items()}
+        accs = {metric_name: metric.get_metric(reset) for metric_name, metric in self.metrics.items()}
+        metric_dict = self.span_metric.get_metric(reset=reset)
+        f1 = {x: y for x, y in metric_dict.items() if "overall" in x}
+        return {**f1, **accs}
 
     @classmethod
     def from_params(cls, vocab: Vocabulary, params: Params) -> 'SimpleTagger':
         embedder_params = params.pop("text_field_embedder")
         text_field_embedder = TextFieldEmbedder.from_params(vocab, embedder_params)
         stacked_encoder = Seq2SeqEncoder.from_params(params.pop("stacked_encoder"))
-
+        label_namespace = params.pop("label_namespace", "labels")
         initializer = InitializerApplicator.from_params(params.pop('initializer', []))
         regularizer = RegularizerApplicator.from_params(params.pop('regularizer', []))
 
         return cls(vocab=vocab,
                    text_field_embedder=text_field_embedder,
                    stacked_encoder=stacked_encoder,
+                   label_namespace=label_namespace,
                    initializer=initializer,
                    regularizer=regularizer)
