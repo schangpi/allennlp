ce4915906fd937b94f64e86065b19941789a6b4c
Soravit Beer Changpinyo <martbeerina@gmail.com>
2018-02-10 01:57:15 -0800

    seq2multiseq (one decoder + task and domain embedding)

--- allennlp/data/dataset_readers/__init__.py
+++ allennlp/data/dataset_readers/__init__.py
@@ -14,5 +14,6 @@ from allennlp.data.dataset_readers.sequence_tagging import SequenceTaggingDatase
 from allennlp.data.dataset_readers.snli import SnliReader
 from allennlp.data.dataset_readers.semantic_role_labeling import SrlReader
 from allennlp.data.dataset_readers.seq2seq import Seq2SeqDatasetReader
+from allennlp.data.dataset_readers.seq2multiseq import Seq2MultiSeqDatasetReader
 from allennlp.data.dataset_readers.coreference_resolution import ConllCorefReader
 from allennlp.data.dataset_readers.penn_tree_bank import PennTreeBankConstituencySpanDatasetReader
