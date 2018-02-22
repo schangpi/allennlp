f5da993cad6f96b19458471b000dae143119da9a
Soravit Beer Changpinyo <martbeerina@gmail.com>
2018-02-21 19:30:51 -0800

    code for reading and generating json files with different hyperparameters

--- json/generate_jsons.py
+++ json/generate_jsons.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 def decode(data, path):
     pathlist = path.split('/')
@@ -7,20 +8,107 @@ def decode(data, path):
         e = e[k]
     return e
 
-choices = {}
-choices["dataset_reader/source_token_indexers/tokens/lowercase_tokens"] = [True, False]
-choices["train_data_path"] = ['./dataset/seq2seq/train/upos_uni_train.txt',
-                              './dataset/seq2seq/train/ner_conll03_train.txt',
-                              './dataset/seq2seq/train/chunk_conll02_train.txt']
-choices["validation_data_path"] = ['./dataset/seq2seq/dev/upos_uni_dev.txt',
-                                   './dataset/seq2seq/dev/ner_conll03_dev.txt',
-                                   './dataset/seq2seq/dev/chunk_conll02_dev.txt']
-choices["test_data_path"] = ['./dataset/seq2seq/test/upos_uni_test.txt',
-                             './dataset/seq2seq/test/ner_conll03_test.txt',
-                             './dataset/seq2seq/test/chunk_conll02_test.txt']
-choices["model/source_embedder/"] = ''
-choices["model/encoder/"] = ''
+def set_data(data, path, value):
+    pathlist = path.split('/')
+    e = data
+    for k in pathlist[:-1]:
+        e = e[k]
+    e[pathlist[-1]] = value
+    return data
 
-with open('seq2seq_template.json', 'r') as f:
+pemb = {
+    'g50':'https://s3-us-west-2.amazonaws.com/allennlp/datasets/glove/glove.6B.50d.txt.gz',
+    'g100':'https://s3-us-west-2.amazonaws.com/allennlp/datasets/glove/glove.6B.100d.txt.gz',
+    'g300':'/data/word_vectors/glove.840B.300d.txt.gz',
+    'f300':'/data/word_vectors/wiki.en.vec.gz'
+}
+pemb_dim = {
+    'g50':50,
+    'g100':100,
+    'g300':300,
+    'f300':300
+}
+
+pemb_gpu = {
+    'g50':0,
+    'g100':0,
+    'g300':1,
+    'f300':2
+}
+
+choices = {}
+# c0
+choices["model/text_field_embedder/tokens/pretrained_file"] = list(pemb.keys())
+word_dim_field = "model/text_field_embedder/tokens/embedding_dim"
+# c1
+choices["model/text_field_embedder/token_characters/embedding/embedding_dim"] = [25]
+# c2
+choices["model/text_field_embedder/token_characters/encoder/input_size"] = [25]
+# c3
+choices["model/text_field_embedder/token_characters/encoder/hidden_size"] = [25, 50, 75]
+# c4
+choices["model/text_field_embedder/token_characters/encoder/dropout"] = [0.1, 0.3, 0.5]
+
+input_size_field = "model/stacked_encoder/input_size"
+# c0 + 2*c3
+
+# c5
+choices["model/stacked_encoder/hidden_size"] = [100, 200, 300, 500]
+# c6
+choices["model/stacked_encoder/dropout"] = [0.1, 0.3, 0.5]
+# c7
+choices["iterator/batch_size"] = [16, 32, 64]
+
+fields = [
+    "model/text_field_embedder/tokens/pretrained_file",
+    "model/text_field_embedder/token_characters/embedding/embedding_dim",
+    "model/text_field_embedder/token_characters/encoder/input_size",
+    "model/text_field_embedder/token_characters/encoder/hidden_size",
+    "model/text_field_embedder/token_characters/encoder/dropout",
+    "model/stacked_encoder/hidden_size",
+    "model/stacked_encoder/dropout",
+    "iterator/batch_size"
+]
+
+all_commands = []
+with open('tagger_crf_template.json', 'r') as f:
     data = json.load(f)
-    print(decode(data, 'dataset_reader/source_token_indexers/tokens/lowercase_tokens'))
+    keys = choices.keys()
+
+    cnt = 0
+    sel = 0
+    for c0 in choices[fields[0]]:
+        data = set_data(data, fields[0], pemb[c0])
+        data = set_data(data, word_dim_field, pemb_dim[c0])
+        for c1 in choices[fields[1]]:
+            data = set_data(data, fields[1], c1)
+            for c2 in choices[fields[2]]:
+                data = set_data(data, fields[2], c2)
+                for c3 in choices[fields[3]]:
+                    data = set_data(data, fields[3], c3)
+                    data = set_data(data, input_size_field, pemb_dim[c0] + 2 * c3)
+                    for c4 in choices[fields[4]]:
+                        data = set_data(data, fields[4], c4)
+                        for c5 in choices[fields[5]]:
+                            data = set_data(data, fields[5], c5)
+                            for c6 in choices[fields[6]]:
+                                data = set_data(data, fields[6], c6)
+                                for c7 in choices[fields[7]]:
+                                    data = set_data(data, fields[7], c7)
+                                    cnt += 1
+                                    print(cnt)
+                                    print(' '.join([str(decode(data, key)) for key in fields]))
+                                    file_ext = '_'.join([str(c) for c in [c0, c1, c2, c3, c4, c5, c6, c7]])
+                                    print(file_ext)
+                                    # with open('tagcrf_' + file_ext +'.json', 'w') as fw:
+                                    #     json.dump(data, fw)
+                                    command = ('mn=' + 'tagcrf_' + file_ext + '\n' +
+                                               'gpu=' + str(pemb_gpu[c0]) + '\n' +
+                                               './tagger.sh ner-iobes_conll03 "$mn" "$gpu"\n')
+                                    if random.random() < 0.25:
+                                        sel += 1
+                                        print(sel)
+                                        all_commands.append(command)
+
+with open('tagcrf_all_commands.txt', 'w') as fw:
+    fw.write('\n\n'.join(all_commands))
