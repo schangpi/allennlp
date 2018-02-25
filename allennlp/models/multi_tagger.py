55ea8538c1fda59cf34030375023eb880fe9f548
Soravit Beer Changpinyo <martbeerina@gmail.com>
2018-02-25 09:09:49 -0800

    Add tag_projection_layer and crf to modules

--- allennlp/models/multi_tagger.py
+++ allennlp/models/multi_tagger.py
@@ -4,13 +4,18 @@ from typing import Dict, Optional
 import numpy
 from overrides import overrides
 import torch
+from torch.nn.modules.linear import Linear
+import torch.nn.functional as F
+from collections import OrderedDict
 
 from allennlp.common import Params
+from allennlp.common.checks import check_dimensions_match
 from allennlp.data import Vocabulary
-from allennlp.modules import Seq2SeqEncoder, TextFieldEmbedder
+from allennlp.modules import Seq2SeqEncoder, TimeDistributed, TextFieldEmbedder, ConditionalRandomField
 from allennlp.models.model import Model
-from allennlp.models.tagger import Tagger
 from allennlp.nn import InitializerApplicator, RegularizerApplicator
+from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
+from allennlp.training.metrics import CategoricalAccuracy, SpanBasedF1Measure
 
 
 @Model.register("multi_tagger")
@@ -68,24 +73,115 @@ class MultiTagger(Model):
         self.label_suffix_namespace = label_suffix_namespace
         self.text_field_embedder = text_field_embedder
         self.stacked_encoder = stacked_encoder
+        self.label_namespaces = OrderedDict()
+        self.tag_projection_layer = OrderedDict()
+        self.num_classes = OrderedDict()
+        self.is_crf = is_crf
+        self.crf = OrderedDict()
+        self.metrics = OrderedDict()
+        self.span_metric = OrderedDict()
+        for tsk in self.tasks:
+            task_label_namespace = tsk + '_' + label_suffix_namespace
+            self.label_namespaces[tsk] = task_label_namespace
+            self.num_classes[tsk] = self.vocab.get_vocab_size(task_label_namespace)
+            self.tag_projection_layer[tsk] = TimeDistributed(Linear(self.stacked_encoder.get_output_dim(),
+                                                                    self.num_classes[tsk]))
+            if is_crf:
+                self.crf[tsk] = ConditionalRandomField(self.num_classes[tsk])
+            self.metrics[tsk] = {
+                "accuracy": CategoricalAccuracy(),
+                "accuracy3": CategoricalAccuracy(top_k=3)
+            }
+            self.span_metric[tsk] = SpanBasedF1Measure(vocab, tag_namespace=task_label_namespace)
+        self.tag_projection_layer = torch.nn.Sequential(self.tag_projection_layer)
+        if is_crf:
+            self.crf = torch.nn.Sequential(self.crf)
+        check_dimensions_match(text_field_embedder.get_output_dim(), stacked_encoder.get_input_dim(),
+                               "text field embedding dim", "encoder input dim")
         initializer(self)
 
-        self.taggers = {}
-        for tsk in self.tasks:
-            print(tsk, torch.cuda.current_device())
-            self.taggers[tsk] = Tagger(vocab=vocab,
-                                       text_field_embedder=text_field_embedder,
-                                       stacked_encoder=stacked_encoder,
-                                       source_namespace=source_namespace,
-                                       label_namespace=tsk + '_' + label_suffix_namespace,
-                                       is_crf=is_crf,
-                                       initializer=initializer,
-                                       regularizer=regularizer)
-        # embed()
-        # Check that parameters are the same for
-        # each self.taggers[tsk].state_dict()['stacked_encoder._module.weight_ih_l0']
-        # And different for
-        # self.taggers[tsk].state_dict()['tag_projection_layer._module.weight']
+    def _examine_source_indices(self, preindices):
+        if not isinstance(preindices, numpy.ndarray):
+            preindices = preindices.data.cpu().numpy()
+        all_predicted_tokens = []
+        for indices in preindices:
+            predicted_tokens = [self.vocab.get_token_from_index(
+                x, namespace=self.source_namespace) for x in list(indices)]
+            all_predicted_tokens.append(predicted_tokens)
+        return all_predicted_tokens
+
+    def _examine_target_indices(self, tsk, preindices):
+        if not isinstance(preindices, numpy.ndarray):
+            preindices = preindices.data.cpu().numpy()
+        all_predicted_tokens = []
+        for indices in preindices:
+            indices = list(indices)
+            # Collect indices till the first end_symbol
+            # if self._end_index in indices:
+            #     indices = indices[:indices.index(self._end_index)]
+            predicted_tokens = [self.vocab.get_token_from_index(
+                x, namespace=self.label_namespaces[tsk]) for x in indices]
+            all_predicted_tokens.append(predicted_tokens)
+        return all_predicted_tokens
+
+    def _print_source_target_triplets(self, tsk, src, tgt, true_tgt):
+        src = self._examine_source_indices(src)
+        true_tgt = self._examine_target_indices(tsk, true_tgt)
+        tgt = self._examine_target_indices(tsk, tgt)
+        for i in [0, int(len(src)/2), -1]:
+            print('Source:      ', ' '.join(src[i]))
+            print('Target:      ', ' '.join(tgt[i]))
+            print('True target: ', ' '.join(true_tgt[i]))
+        print('')
+
+    def task_forward(self,
+                     tsk_id,
+                     tsk,
+                     tokens: Dict[str, torch.LongTensor],
+                     tags: torch.LongTensor = None) -> Dict[str, torch.Tensor]:
+        embedded_text_input = self.text_field_embedder(tokens)
+        mask = get_text_field_mask(tokens)
+        encoded_text = self.stacked_encoder(embedded_text_input, mask)
+        logits = self.tag_projection_layer[tsk_id].forward(encoded_text) # (batch_size, sequence_length, num_classes)
+        if self.is_crf:
+            predicted_tags = self.crf[tsk_id].viterbi_tags(logits, mask)
+        else:
+            reshaped_log_probs = logits.view(-1, self.num_classes[tsk])
+            batch_size, sequence_length, _ = embedded_text_input.size()
+            class_probabilities = F.softmax(reshaped_log_probs, dim=-1).view(
+                [batch_size, sequence_length, self.num_classes[tsk]])
+            all_predictions = class_probabilities.cpu().data.numpy()
+            if all_predictions.ndim == 3:
+                predictions_list = [all_predictions[i] for i in range(all_predictions.shape[0])]
+            else:
+                predictions_list = [all_predictions]
+            predicted_tags = []
+            for predictions in predictions_list:
+                argmax_indices = numpy.argmax(predictions, axis=-1)
+                predicted_tags.append(argmax_indices)
+        output_dict = {"logits": logits,
+                       "tags": predicted_tags,
+                       "mask": mask}
+        if tags is not None:
+            loss = 0.0
+            if self.is_crf:
+                log_likelihood = self.crf[tsk_id](logits, tags, mask)
+                loss = -log_likelihood
+            else:
+                loss = sequence_cross_entropy_with_logits(logits, tags, mask)
+            for metric in self.metrics[tsk].values():
+                metric(logits, tags, mask.float())
+            output_dict["loss"] = loss
+
+            # Represent viterbi tags as "class probabilities" that we can
+            # feed into the `span_metric`
+            class_probabilities = logits * 0.
+            for i, instance_tags in enumerate(predicted_tags):
+                for j, tag_id in enumerate(instance_tags):
+                    class_probabilities[i, j, tag_id] = 1
+            self.span_metric[tsk](class_probabilities, tags, mask)
+            self._print_source_target_triplets(tsk, tokens['tokens'], numpy.array(predicted_tags), tags)
+        return output_dict
 
     @overrides
     def forward(self,  # type: ignore
@@ -149,7 +245,7 @@ class MultiTagger(Model):
                     task_tokens[k] = tokens[k][task_indices, :]
             task_all_tags = all_tags[:, i, :].squeeze()
             # embed()
-            task_output_dict = self.taggers[tsk].forward(task_tokens, task_all_tags[task_indices, :].contiguous())
+            task_output_dict = self.task_forward(i, tsk, task_tokens, task_all_tags[task_indices, :].contiguous())
             task_tags = task_output_dict['tags']
             for j, ti in enumerate(task_indices):
                 predictions[ti] = task_tags[j]
@@ -158,6 +254,13 @@ class MultiTagger(Model):
         output_dict = {'loss': loss,
                        'tags': predictions,
                        'label_namespaces': label_namespaces}
+        return output_dict
+
+    def task_decode(self, tsk, output_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        output_dict["tags"] = [
+            [self.vocab.get_token_from_index(tag, namespace=self.label_namespaces[tsk])
+             for tag in instance_tags]
+            for instance_tags in output_dict["tags"]]
         return output_dict
 
     @overrides
@@ -169,7 +272,8 @@ class MultiTagger(Model):
         """
         task_output_dicts = {}
         for tsk in self.tasks:
-            task_output_dicts[tsk + '_labels'] = self.taggers[tsk].decode(output_dict)
+            tmp_output_dict = output_dict.copy()
+            task_output_dicts[tsk + '_labels'] = self.task_decode(tsk, tmp_output_dict)
         all_predicted_tokens = []
         for b, task_namespace in enumerate(output_dict['label_namespaces']):
             all_predicted_tokens.append(task_output_dicts[task_namespace]['tags'][b])
@@ -178,24 +282,24 @@ class MultiTagger(Model):
 
     @overrides
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
-        task_accs = {}
+        # task_accs = {}
         task_f1s = {}
         for tsk in self.tasks:
-            # for metric_name, metric in self.taggers[tsk].metrics.items():
+            # for metric_name, metric in self.metrics[tsk].items():
             #     task_accs[tsk + '-' + metric_name] = metric.get_metric(reset)
-            metric_dict = self.taggers[tsk].span_metric.get_metric(reset=reset)
+            metric_dict = self.span_metric[tsk].get_metric(reset=reset)
             for x, y in metric_dict.items():
                 # if "overall" in x:
                 if "f1-measure-overall" in x:
                     task_f1s[tsk + '-' + x] = y
         # avg_accs = {}
         avg_f1s = {}
-        # for metric_name, _ in self.taggers[self.tasks[0]].metrics.items():
+        # for metric_name, _ in self.metrics[self.tasks[0]].items():
             # total_acc = []
             # for tsk in self.tasks:
             #     total_acc.append(task_accs[tsk + '-' + metric_name])
             # avg_accs[metric_name] = sum(total_acc) / len(total_acc)
-        for x, _ in self.taggers[self.tasks[0]].span_metric.get_metric(reset=reset).items():
+        for x, _ in self.span_metric[self.tasks[0]].get_metric(reset=reset).items():
             # if "overall" in x:
             if "f1-measure-overall" in x:
                 total_f1 = []
@@ -204,11 +308,6 @@ class MultiTagger(Model):
                 avg_f1s[x] = sum(total_f1) / len(total_f1)
         # return {**avg_f1s, ** avg_accs, **task_f1s, **task_accs, }
         return {**avg_f1s, **task_f1s}
-
-    @overrides
-    def set_device(self, device):
-        for tsk in self.tasks:
-            self.taggers[tsk] = self.taggers[tsk].cuda(device)
 
     @classmethod
     def from_params(cls, vocab: Vocabulary, params: Params) -> 'MultiTagger':
