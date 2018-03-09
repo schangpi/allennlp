ed65396d7a20d45fbe12aece49bd4c5747ac4dd9
Soravit Beer Changpinyo <martbeerina@gmail.com>
2018-03-08 16:38:51 -0800

    new script

---
+++ tagger_clean.sh
@@ -0,0 +1,18 @@
+DATADIR=./dataset/multi_tagger
+TMPDIR=/data/beer/tagger_clean
+TASK_NAME=$1
+MODEL=$2
+CUDA=$3
+
+echo "$TASK_NAME"
+echo "$MODEL"
+echo "$CUDA"
+
+rm -r "$TMPDIR"/"$TASK_NAME"_"$MODEL"
+python -m allennlp.run train json/single/"$MODEL".json \
+--serialization-dir "$TMPDIR"/"$TASK_NAME"_"$MODEL" \
+--cuda-device "$CUDA" \
+--train "$DATADIR"/train/"$TASK_NAME"_train.txt \
+--dev "$DATADIR"/dev/"$TASK_NAME"_dev.txt \
+--test "$DATADIR"/test/"$TASK_NAME"_test.txt \
+ > "$TMPDIR"/"$TASK_NAME"_"$MODEL"_log
