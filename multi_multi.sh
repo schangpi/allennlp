27a1aca8f230174f9dbd4de94af20947ddf82174
Soravit Beer Changpinyo <martbeerina@gmail.com>
2018-03-13 00:19:52 -0700

    scripts

---
+++ multi_multi.sh
@@ -0,0 +1,20 @@
+TASK_EXT=$1
+TMPDIR=$2
+CUDA=$3
+
+DATADIR=./dataset/multi_tagger_clean
+
+for ext in "$TASK_EXT"
+do
+JSONDIR=multi
+TASK_NAME=multitagger
+MODEL="$JSONDIR"_"$ext"
+rm -r "$TMPDIR"/"$TASK_NAME"_"$MODEL"
+python -m allennlp.run train json/"$JSONDIR"/"$MODEL".json \
+--serialization-dir "$TMPDIR"/"$TASK_NAME"_"$MODEL" \
+--cuda-device "$CUDA" \
+--train "$DATADIR"/train \
+--dev "$DATADIR"/dev \
+--test "$DATADIR"/test \
+ > "$TMPDIR"/"$TASK_NAME"_"$MODEL"_log 
+done
---
+++ multi_tpeonly.sh
@@ -0,0 +1,21 @@
+TASK_EXT=$1
+TMPDIR=$2
+CUDA=$3
+
+DATADIR=./dataset/multi_tagger_clean
+
+for ext in "$TASK_EXT"
+do
+JSONDIR=taskonly_prepend_embedding
+TASK_NAME=taskembtagger
+MODEL="$JSONDIR"_tagger_"$ext"
+rm -r "$TMPDIR"/"$TASK_NAME"_"$MODEL"
+python -m allennlp.run train json/"$JSONDIR"/"$MODEL".json \
+--serialization-dir "$TMPDIR"/"$TASK_NAME"_"$MODEL" \
+--cuda-device "$CUDA" \
+--train "$DATADIR"/train \
+--dev "$DATADIR"/dev \
+--test "$DATADIR"/test \
+ > "$TMPDIR"/"$TASK_NAME"_"$MODEL"_log
+ 
+done
